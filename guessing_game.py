67af18f2f3ce3b08aa483e996aa75a006c5d6b55
PranavBidwe3669 <pranavbidwe1@gmail.com>
2025-06-09 04:45:37 +0530

    updated

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,14 @@
-print("hello")
+secret_number = 10
+guess_count = 1
+guess_limit = 3
+print("welcome to the guessing number game..")
+print("you have got 3 chances to guess the correct number..")
+user_input = int(input("guess the number from 1 to 10: "))
+while guess_count < guess_limit:
+    user_input = int(input("take a guess: "))
+    guess_count += 1
+    if user_input == secret_number:
+        print("you won! ")
+        break
+else:
+    print("you have lost the game! ")
